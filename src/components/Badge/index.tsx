1aebd6d3baacd60b417140c37acc2b853fe005a4
Renzo Telenta <renzotelenta@gmail.com>
2021-01-24 02:29:12 -0500

    feat(badge): basic badge

---
+++ src/components/Badge/index.tsx
@@ -0,0 +1,26 @@
+import { Flex, Box } from "@chakra-ui/react";
+
+interface IBadgeProps {
+  bg: string;
+  icon: React.ReactNode;
+}
+
+const Badge: React.FC<IBadgeProps> = ({ children, bg, icon }) => {
+  return (
+    <Flex
+      rounded="full"
+      px={3}
+      py={1}
+      color="white"
+      boxShadow="lg"
+      fontSize="sm"
+      textTransform="uppercase"
+      bg={bg}
+      alignItems="center"
+    >
+      <Box mr={1}>{children}</Box> {icon}
+    </Flex>
+  );
+};
+
+export default Badge;
